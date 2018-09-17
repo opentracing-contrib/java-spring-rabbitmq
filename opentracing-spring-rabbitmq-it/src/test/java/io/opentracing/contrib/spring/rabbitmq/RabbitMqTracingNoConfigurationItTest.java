a2a84e20e0c30d3a02c06468fcc88ecf1304fd9a
Ats Uiboupin <ats.uiboupin@iglu.ee>
2018-09-17 08:38:12 +0300

    issue #8 - fix(doc): replace incorrect @author (due to copy-paste error)

--- opentracing-spring-rabbitmq-it/src/test/java/io/opentracing/contrib/spring/rabbitmq/RabbitMqTracingNoConfigurationItTest.java
+++ opentracing-spring-rabbitmq-it/src/test/java/io/opentracing/contrib/spring/rabbitmq/RabbitMqTracingNoConfigurationItTest.java
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
- * @author Gilles Robert
+ * @author Ats Uiboupin
  */
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.NONE,
